5f180ee23bb5de7d4edddb0f854c89e5bad83c14
Miguel Vadillo <vadillo@ti.com>
2010-05-24 18:19:20 -0500

    SYSLINK:IPU-PM: enabling by default ipu_pm
    - ipu-pm resource framework is now enable by default
    - ipu-pm notification (SUSPEND/RESUME) is still enable
      in compilation time in the menuconfig and is only afecting
      code in proc4430.c
    - some cosmetic changes in the code
    - include the new ipu_pm.h file.
    - change the names of some functions to match ipu_pm.h
    
    Signed-off-by: Miguel Vadillo <vadillo@ti.com>
    Signed-off-by: Paul Hunt <phunt@ti.com>

--- drivers/dsp/syslink/multicore_ipc/platform.c
+++ drivers/dsp/syslink/multicore_ipc/platform.c
@@ -28,6 +28,8 @@
 
 /* SysLink device specific headers */
 #include "../procmgr/proc4430/proc4430.h"
+#include "../ipu_pm/ipu_pm.h"
+
 
 /* Module level headers */
 #include <multiproc.h>
@@ -231,10 +233,8 @@
  */
 void *platform_notifydrv_handle;
 
-#ifdef CONFIG_SYSLINK_DUCATI_PM
 struct pm_event *pm_event;
-struct sms *rcb_struct;
-#endif
+struct sms *rcb_table;
 
 /* Handles for SysM3 */
 void *platform_nsrn_gate_handle_sysm3;
@@ -949,7 +949,8 @@ void platform_start_callback(void *arg)
 				goto notify_ducatidrv_create_fail;
 			}
 		}
-#ifdef CONFIG_SYSLINK_DUCATI_PM
+		/* BEGIN PM */
+
 		/* Ducati PwrMgmt events */
 		/* Only registered for APPM3 FIXME?*/
 
@@ -958,7 +959,7 @@ void platform_start_callback(void *arg)
 				platform_notifydrv_handle,
 				proc_id,/*DUCATI_PROC*/
 				PM_RESOURCE,/*PWR_MGMT_EVENT*/
-				(notify_callback_fxn)proc4430_drv_pm_callback,
+				(notify_callback_fxn)ipu_pm_callback,
 				(void *)NULL);
 			if (status < 0)
 				goto pm_register_fail;
@@ -966,16 +967,14 @@ void platform_start_callback(void *arg)
 				platform_notifydrv_handle,
 				proc_id,/*DUCATI_PROC*/
 				PM_NOTIFICATION,/*PWR_MGMT_EVENT*/
-				(notify_callback_fxn)
-					proc4430_drv_pm_notify_callback,
+				(notify_callback_fxn)ipu_pm_notify_callback,
 				(void *)NULL);
 			if (status < 0) {
 				status = notify_unregister_event(
 				platform_notifydrv_handle,
 				proc_id,/*DUCATI_PROC*/
 				PM_RESOURCE,/*PWR_MGMT_EVENT*/
-				(notify_callback_fxn)
-					proc4430_drv_pm_notify_callback,
+				(notify_callback_fxn)ipu_pm_notify_callback,
 				(void *)NULL);
 				if (status < 0)
 					printk(KERN_INFO
@@ -984,7 +983,7 @@ void platform_start_callback(void *arg)
 			}
 
 			/* Get the shared RCB */
-			rcb_struct = (struct sms *) ioremap(PM_SHM_BASE_ADDR,
+			rcb_table = (struct sms *) ioremap(PM_SHM_BASE_ADDR,
 				sizeof(struct sms));
 
 			pm_event =
@@ -997,11 +996,10 @@ void platform_start_callback(void *arg)
 					kzalloc(sizeof(struct semaphore),
 						GFP_KERNEL);
 				sema_init(pm_event[i].sem_handle, 0);
-				pm_event[i].eventType = i;
+				pm_event[i].event_type = i;
 			}
 		}
 		/* END PM */
-#endif
 
 		/* The notify is created only once and used for Sys and App */
 		if (index == SMHEAP_SRINDEX_APPM3)
@@ -1314,11 +1312,9 @@ multiproc_fail:
 proc_invalid_id:
 	printk(KERN_ERR "platform_load_callback failed invalid"
 			" proc_id [0x%x]\n", proc_id);
-#ifdef CONFIG_SYSLINK_DUCATI_PM
 	goto exit;
 pm_register_fail:
 	printk(KERN_ERR "pm register events failed");
-#endif
 exit:
 	return;
 }
@@ -1338,9 +1334,7 @@ void platform_stop_callback(void *arg)
 	u16 proc_id = (u32) arg;
 	int index = 0;
 	u32 nread = 0;
-	#ifdef CONFIG_SYSLINK_DUCATI_PM
 	u32 i = 0;
-	#endif
 
 	if (proc_id == multiproc_get_id("SysM3"))
 		index = SMHEAP_SRINDEX_SYSM3;
@@ -1447,36 +1441,35 @@ void platform_stop_callback(void *arg)
 		else
 			platform_notifydrv_handle_sysm3 = NULL;
 
-#ifdef CONFIG_SYSLINK_DUCATI_PM
-		/* PM */
+		/* BEGIN PM */
+
 		if (index == SMHEAP_SRINDEX_APPM3) {
 			status = notify_unregister_event(
 				platform_notifydrv_handle,
 				proc_id,/*DUCATI_PROC*/
 				PM_RESOURCE,/*PWR_MGMT_EVENT*/
-				(notify_callback_fxn)proc4430_drv_pm_callback,
+				(notify_callback_fxn)ipu_pm_callback,
 				(void *)NULL);
 			if (status < 0)
-				printk(KERN_INFO "*****ERROR UNREGISTERING PM EVENT\n");
+				printk(KERN_INFO "ERROR UNREGISTERING PM EVENT\n");
 			status = notify_unregister_event(
 				platform_notifydrv_handle,
 				proc_id,/*DUCATI_PROC*/
 				PM_NOTIFICATION,/*PWR_MGMT_EVENT*/
-				(notify_callback_fxn)
-					proc4430_drv_pm_notify_callback,
+				(notify_callback_fxn)ipu_pm_notify_callback,
 				(void *)NULL);
 			if (status < 0)
-				printk(KERN_INFO "*****ERROR UNREGISTERING PM EVENT\n");
+				printk(KERN_INFO "ERROR UNREGISTERING PM EVENT\n");
 			for (i = 0; i < NUMBER_PM_EVENTS; i++) {
 				kfree(pm_event[i].sem_handle);
-				pm_event[i].eventType = 0;
+				pm_event[i].event_type = 0;
 			}
 			kfree(pm_event);
 			/* Release the shared RCB */
-			iounmap(rcb_struct);
+			iounmap(rcb_table);
 		}
 		/* END PM */
-#endif
+
 		if (platform_notifydrv_handle_sysm3 == NULL &&
 				platform_notifydrv_handle_appm3 == NULL) {
 			status = notify_ducatidrv_delete(
