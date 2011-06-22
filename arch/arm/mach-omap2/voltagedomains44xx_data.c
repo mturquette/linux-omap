410f5db4cc052c0bd6eb533c81b32330b05388f8
Mike Turquette <mturquette@ti.com>
2011-06-22 10:43:52 -0700

    don't initialize abb for vdd_iva

--- arch/arm/mach-omap2/voltagedomains44xx_data.c
+++ arch/arm/mach-omap2/voltagedomains44xx_data.c
@@ -74,7 +74,6 @@ static struct voltagedomain omap4_voltdm_iva = {
 	.vc = &omap4_vc_iva,
 	.vfsm = &omap4_vdd_iva_vfsm,
 	.vp = &omap4_vp_iva,
-	.abb = &omap4_abb_iva,
 	.vdd = &omap4_vdd_iva_info,
 };
 
